3d25b856fc1e5978e59ba4d2cacea76060bbbb48
umisuzume-sora <airsteamauk@gmail.com>
2021-11-21 14:09:53 +0000

    add column

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,58 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
+  　@tasks = Task.all
+  end
+
+　def show
+　end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'タスクが投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクが投稿されません'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end  
+  end
+
+  def destroy
+    @task.destroy
+    
+    flash[:success] = 'タスクが削除されました'
+    redirect_to tasks_url
+
+  end
+  
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end  
+  
+  def task_params
+  params.require(:task).permit(:content, :status)
+  end
+end
